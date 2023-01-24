24bb1c31161a2c7f7bf7f8147500dbb9067c282d
RD <renatafd717@gmail.com>
2023-01-23 22:45:21 -0600

    moving away from heroku

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export default {
-    REACT_APP_API_ENDPOINT: 'https://dsa-petful-renata.herokuapp.com/'
-    //REACT_APP_API_ENDPOINT: 'http://localhost:8080/' 
-
-}
+  REACT_APP_API_ENDPOINT: "https://web-production-bd73.up.railway.app/",
+  //REACT_APP_API_ENDPOINT: 'http://localhost:8080/'
+};
